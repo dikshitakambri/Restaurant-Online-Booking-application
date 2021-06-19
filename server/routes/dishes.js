344f5ee23320b471a7e913d22885a9a0f57773a1
dikshitakambri <dikshitakambri18@gmail.com>
2021-06-19 12:22:08 +0530

    cors

--- server/routes/dishes.js
+++ server/routes/dishes.js
@@ -7,20 +7,20 @@ var dishController = require("../Controllers/dish");
 router.use(bodyParser.json());
 
 /* GET home page. */
-router.get('/', dishController.getDishes);
-router.post("/",passportJWT.verifyUser,passportJWT.verifyAdmin, dishController.addDish);
-router.get("/:dishId",passportJWT.verifyUser,passportJWT.verifyAdmin, dishController.getDish);
-router.put("/:dishId",passportJWT.verifyUser,passportJWT.verifyAdmin, dishController.updateDish);
-router.delete("/:dishId",passportJWT.verifyUser,passportJWT.verifyAdmin, dishController.deleteDish);
+router.get('/',cors.corsWithOptions, dishController.getDishes);
+router.post("/",cors.corsWithOptions,passportJWT.verifyUser,passportJWT.verifyAdmin, dishController.addDish);
+router.get("/:dishId",cors.corsWithOptions,passportJWT.verifyUser,passportJWT.verifyAdmin, dishController.getDish);
+router.put("/:dishId",cors.corsWithOptions,passportJWT.verifyUser,passportJWT.verifyAdmin, dishController.updateDish);
+router.delete("/:dishId",cors.corsWithOptions,passportJWT.verifyUser,passportJWT.verifyAdmin, dishController.deleteDish);
 
 router.route("/:dishId/comments")
-.get(dishController.getComments)
-.post(passportJWT.verifyUser,dishController.addComments)
-.delete(passportJWT.verifyUser,passportJWT.verifyAdmin,dishController.deleteComments);
+.get(cors.corsWithOptions,dishController.getComments)
+.post(cors.corsWithOptions,passportJWT.verifyUser,dishController.addComments)
+.delete(cors.corsWithOptions,passportJWT.verifyUser,passportJWT.verifyAdmin,dishController.deleteComments);
 
 router.route("/:dishId/comments/:commentId")
-.get(dishController.getCommentById)
-.put(passportJWT.verifyUser,dishController.updateComment)
-.delete(passportJWT.verifyUser,dishController.deletecommentById);
+.get(cors.corsWithOptions,dishController.getCommentById)
+.put(cors.corsWithOptions,passportJWT.verifyUser,dishController.updateComment)
+.delete(cors.corsWithOptions,passportJWT.verifyUser,dishController.deletecommentById);
 
 module.exports = router;
